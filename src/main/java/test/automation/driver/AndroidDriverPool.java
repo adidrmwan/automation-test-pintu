2e296a5ea21c012568b9793a7b06dc892c5f9907
Adi Darmawan <adi.darmawan@shipper.id>
2022-10-01 01:06:33 +0700

    make appium start automatically

--- src/main/java/test/automation/driver/AndroidDriverPool.java
+++ src/main/java/test/automation/driver/AndroidDriverPool.java
@@ -4,6 +4,9 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import lombok.Data;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +15,9 @@ import test.automation.properties.ConfigProperties;
 import test.automation.properties.DriverAndroidProperties;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.ServerSocket;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
@@ -25,12 +30,14 @@ public class AndroidDriverPool {
     @Autowired
     ConfigProperties configProperties;
 
+    private AppiumDriverLocalService service;
+    private AppiumServiceBuilder builder;
 
     public AndroidDriver create() {
+        startAppiumServer();
+
         AndroidDriver<AndroidElement> driver = null;
         DesiredCapabilities caps = new DesiredCapabilities();
-
-        String appiumUrl = driverAndroidProperties.getAppiumUrl();
 
         caps.setCapability(
                 AndroidMobileCapabilityType.PLATFORM_NAME, driverAndroidProperties.getPlatformName());
@@ -42,12 +49,38 @@ public class AndroidDriverPool {
         caps.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 100);
         caps.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir") + File.separator + configProperties.getAndroid().getAppPath());
 
+        driver = new AndroidDriver<AndroidElement>(service.getUrl(), caps);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        return driver;
+    }
+
+    public void startAppiumServer() {
+        builder = new AppiumServiceBuilder();
+        builder.withIPAddress("127.0.0.1");
+        builder.usingPort(getAvailablePort());
+        builder.withArgument(GeneralServerFlag.SESSION_OVERRIDE);
+        builder.withArgument(GeneralServerFlag.LOG_LEVEL,"error");
+
+        //Start the server with the builder
+        service = AppiumDriverLocalService.buildService(builder);
+        service.start();
+    }
+
+    public static int getAvailablePort() {
+        int port = 4723;
+
         try {
-            driver = new AndroidDriver<AndroidElement>(new URL(appiumUrl), caps);
-            driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        } catch (MalformedURLException e) {
+            ServerSocket serverSocket = new ServerSocket(0);
+            port = serverSocket.getLocalPort();
+            serverSocket.close();
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        return driver;
+        return port;
+    }
+
+    public void stopAppiumServer() {
+        service.stop();
     }
 }
--- src/test/java/test/automation/hooks/BaseDriverHooks.java
+++ src/test/java/test/automation/hooks/BaseDriverHooks.java
@@ -2,6 +2,7 @@ package test.automation.hooks;
 
 import io.cucumber.java.After;
 import org.springframework.beans.factory.annotation.Autowired;
+import test.automation.driver.AndroidDriverPool;
 import test.automation.driver.DriverPool;
 
 import java.util.Optional;
@@ -10,19 +11,21 @@ public class BaseDriverHooks {
     @Autowired
     DriverPool driverPool;
 
+    @Autowired
+    AndroidDriverPool androidDriverPool;
+
     @After
     public void afterTest() {
-        quitWebdrivers(driverPool);
+        quitDrivers(driverPool);
     }
 
-    public void quitWebdrivers(DriverPool drivers) {
+    public void quitDrivers(DriverPool drivers) {
         Optional.ofNullable(drivers.getAndroidDriver())
                 .ifPresent(
                         driver -> {
                             driver.quit();
+                            androidDriverPool.stopAppiumServer();
                             drivers.setAndroidDriver(null);
                         });
     }
-
-
 }
